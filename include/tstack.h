ac7bf5ebe978d2a23f356bd5edc4f346e7c01fc6
pabold1337 <apa2007nn@gmail.com>
2026-04-18 02:38:51 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,11 +4,11 @@
 
 template<typename T, int size>
 class TStack {
-private:
+ private:
     T arr[size];
     int top_idx;
 
-public:
+ public:
     TStack() : top_idx(-1) {}
 
     void push(T value) {
